ec9960880f2f7247483601f9cc673e89afaa1971
soubinkittiphanh <kittiphanhsoubin@gmail.com>
2021-08-24 15:52:46 +0700

    new devssss

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 const buildApp = require("./models/app.js");
 const conf=require("./config");
 
-const startApp =  () => {
+const startApp = async () => {
   const appOption = {
     logger: true,
   }
 
-  const app = buildApp(appOption);
+  const app = await buildApp(appOption);
+  app.listen(conf.port,()=>{
+    console.log("Your app is runing  :"+conf.port);
+  })
 
-  try {
-    // await app.listen(conf.port, conf.host); //this format is not working
-     app.listen(conf.port, conf.host);
-    console.log("App is running on port:++ " + conf.port+' | '+conf.db);
-  } catch (error) {
-    throw error;
-  }
+  // try {
+  //   // await app.listen(conf.port, conf.host); //this format is not working
+  //    app.listen(conf.port, conf.host);
+  //   console.log("App is running on port:++ " + conf.port+' | '+conf.db);
+  // } catch (error) {
+  //   throw error;
+  // }
 };
 
 startApp();
