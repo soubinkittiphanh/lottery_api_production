a8417e533947dde0e36dca9a58fd384d1fffe529
soubinkittiphanh <kittiphanhsoubin@gmail.com>
2021-08-24 17:58:21 +0700

    promisefinal

--- src/config/dbconn.js
+++ src/config/dbconn.js
@@ -1,7 +1,5 @@
 const mysql = require("mysql");
 const env=require('../config');
-// const con = require("../../package.json");
-
 const db=mysql.createConnection({
   host: env.db.host,
   user: env.db.user,
@@ -9,11 +7,4 @@ const db=mysql.createConnection({
   database: env.db.database,
   port: env.db.port,
 });
-// const db = mysql.createConnection({
-//   host: con.dbConf.host,
-//   user: con.dbConf.user,
-//   password: con.dbConf.password,
-//   database: con.dbConf.database,
-//   port: con.dbConf.port,
-// });
 module.exports = db;
